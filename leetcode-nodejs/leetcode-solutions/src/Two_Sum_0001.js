178e8887ffeb913df6d3ee6c80860ab3e77368cb
Connor Keenum <Nellakprime@gmail.com>
2022-05-29 15:00:07 -0500

    Created Two_Sum_0001 using TDD.

--- leetcode-nodejs/leetcode-solutions/src/Two_Sum_0001.js
+++ leetcode-nodejs/leetcode-solutions/src/Two_Sum_0001.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+
+const twoSum = (nums, target) => {
+    /** 
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    const result = {}
+    for (let i = 0; i < nums.length; i++) {
+        const value = nums[i]          // It is the value in the array at the current index
+        const diff = target - value    // It is the difference between target and current value
+        if (result[value] !== undefined) { return [result[value], i] } 
+        else { result[diff] = i }
+    }
+    return [];                         // If there is no solution, return an empty array
+}
+
+module.exports = twoSum;
